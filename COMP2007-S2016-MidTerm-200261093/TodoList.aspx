88b1d92faaadece1adabcf3e46cae9ec89a948f5
AustinGCState <austinmkpc@hotmail.com>
2016-06-23 17:56:52 -0400

    Add project files.

---
+++ COMP2007-S2016-MidTerm-200261093/TodoList.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Title="Todo List" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="TodoList.aspx.cs" Inherits="COMP2007_S2016_MidTerm_200261093.TodoList" %>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-offset-2 col-md-8">
+                <h1>Todo List</h1>
+                <asp:GridView runat="server" CssClass="table table-bordered table-striped table-hover" ID="TodoGridview" AutoGenerateColumns="false">
+                    <Columns>
+                        <asp:BoundField DataField="TodoName" HeaderText="Todo" Visible="true" />
+                        <asp:BoundField DataField="TodoNotes" HeaderText="Todo" Visible="true" />
+                        <asp:BoundField DataField="Completed" HeaderText="Todo" Visible="true" />
+                    </Columns>
+                </asp:GridView>
+            </div>
+        </div>
+    </div>
+
+
+</asp:Content>
---
+++ COMP2007-S2016-MidTerm-200261093/TodoList.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+// using statemtents we need to connect to EF DB
+using COMP2007_S2016_MidTerm_200261093.Models;
+using System.Web.ModelBinding;
+
+namespace COMP2007_S2016_MidTerm_200261093
+{
+    public partial class TodoList : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //if page is being oaded for the first time, populate to do
+            if (!IsPostBack) {
+                //get todo data
+                this.GetTodo();
+            }
+        }
+        
+        // * This method gets the student data from the DB
+        //method GetTodo
+        //returns {void}
+        
+        protected void GetTodo() {
+
+            //connect to EF
+            using (TodoConnection db = new TodoConnection()) {
+
+                var Todo = (from allTodo in db.Todos select allTodo);
+
+                //bind the result to the Gridview
+                TodoGridview.DataSource = Todo.ToList();
+                TodoGridview.DataBind();
+            }
+        }
+
+    }
+}
